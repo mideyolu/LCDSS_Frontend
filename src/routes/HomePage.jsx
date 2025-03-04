e408ba57b819642272ac7b4b2effc0fd0056f257
mideyolu <olumideoluwuyi91@gmail.com>
2025-03-04 22:52:32 +0100

    Updated HomePage.jsx

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -2,10 +2,10 @@ import FeaturesSection from "../components/HomePage/FeaturesSection";
 import HeroSection from "../components/HomePage/HeroSection";
 const HomePage = () => {
     return (
-        <>
+        <div className=" mx-[1.5rem]">
             <HeroSection />
             <FeaturesSection />
-        </>
+        </div>
     );
 };
 
