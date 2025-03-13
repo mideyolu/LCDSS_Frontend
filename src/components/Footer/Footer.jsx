7b32d23fac8d6fa5ebd43d215a3ba0df3294dbf2
mideyolu <olumideoluwuyi91@gmail.com>
2025-03-13 18:59:05 +0100

    Updated Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import { Layout, Typography } from "antd";
 import React from "react";
 import { FaGithub } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = ({ className }) => {
     const { Text } = Typography;
+    const location = useLocation(); // Get current route
+
+    const isDashboard = location.pathname === "/dashboard"; // Check if user is on dashboard
+
     return (
         <Layout
             style={{
@@ -19,14 +23,16 @@ const Footer = ({ className }) => {
                 }}
                 className="flex flex-col text-center md:flex-row items-center justify-between"
             >
-                © {new Date().getFullYear()} Respirix Healthcare Dashboard. All
-                rights reserved. <br />
-                <strong className="flex mt-3 md:mt-0 items-center gap-5">
-                    Made by Project Group 9{" "}
-                    <Link to={"https://github.com/mideyolu/LCDSS"}>
-                        <FaGithub size={18} style={{ color: "#000" }} />
-                    </Link>
-                </strong>
+                © {new Date().getFullYear()} Respirix Healthcare. All rights
+                reserved. <br />
+                {isDashboard && ( // Conditionally render if on /dashboard
+                    <strong className="flex mt-3 md:mt-0 items-center gap-5">
+                        Dashboard | Made by Project Group 9{" "}
+                        <Link to={"https://github.com/mideyolu/LCDSS"}>
+                            <FaGithub size={18} style={{ color: "#000" }} />
+                        </Link>
+                    </strong>
+                )}
             </Text>
         </Layout>
     );
